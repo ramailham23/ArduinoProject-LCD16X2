58d202ee4cbce203407d8a3f7b5fa4a1086b6be7
Moh. Ilham Ramadan <mohilhamramadana721@gmail.com>
2025-02-26 17:53:13 +0700

    Update and rename helloworld.ino to BlinkingText.ino

--- BlinkingText.ino
+++ BlinkingText.ino
@@ -1,5 +1,5 @@
 #include <LiquidCrystal.h>
-LiquidCrystal lcd(12,11,5,4,3,2);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   lcd.begin(16,2);
@@ -9,6 +9,6 @@ void setup() {
 void loop() {
   lcd.noDisplay();
   delay(500);
-  lcd.display(); 
+  lcd.display();
   delay(500);
 }

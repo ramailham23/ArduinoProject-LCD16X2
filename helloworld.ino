865441d95fa8e3ac30b80cd98746993eef33eac4
Moh. Ilham Ramadan <mohilhamramadana721@gmail.com>
2025-02-26 17:31:24 +0700

    Create helloworld.ino

---
+++ helloworld.ino
@@ -0,0 +1,14 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12,11,5,4,3,2);
+
+void setup() {
+  lcd.begin(16,2);
+  lcd.print("Hello, World");
+}
+
+void loop() {
+  lcd.noDisplay();
+  delay(500);
+  lcd.display(); 
+  delay(500);
+}
